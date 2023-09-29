10260aa8cf6b59bdf91bf6ab25bb8ff898db9104
Robert Mikolajczyk <ronert1674@gmail.com>
2023-09-29 20:50:37 +0200

    add deal_cards function

--- main.py
+++ main.py
@@ -9,3 +9,15 @@
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
+
+import random
+
+
+def deal_cards() -> int:
+    """Function that uses the list of cards to return a random card."""
+    cards = [11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+    return random.choice(cards)
+
+
+print(deal_cards())
